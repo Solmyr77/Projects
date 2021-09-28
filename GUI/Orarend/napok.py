d11de1435594035204467cf885c2a931584ad646
Solmyr </>
2021-09-28 21:01:05 +0200

    Initial commit

--- GUI/Orarend/napok.py
+++
@@ -1,7 +0,0 @@
-list_hetfo = ['Töri', 'IKT', 'Matek', 'Magyar', 'Infotáv', 'Infotáv', 'Tesi', 'Tesi']
-list_kedd = ['Angol', 'IKT', 'IKT', 'Magyar', 'Fizika', 'Matek']
-list_szerda = ['Progalap', 'Progalap', 'Ofő', 'Magyar', 'Matek', 'Pénzügy', 'Angol']
-list_csut = ['Infotáv', 'Infotáv', 'Fizika', 'Töri', 'Tesi', 'Angol', 'Magyar', 'Digitkult']
-list_pentek = ['Matek', 'Töri', 'Angol', 'Magyar', 'Tesi']
-
-gomblist = ['gomb1', 'gomb2', 'gomb3', 'gomb4', 'gomb5']
