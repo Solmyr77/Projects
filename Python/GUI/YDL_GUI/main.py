820d966d3c669e00e169dffe3c5f43ac3a94ae1b
Solmyr77 <91564294+Solmyr77@users.noreply.github.com>
2021-11-18 20:51:24 +0100

    Sync

---
+++ Python/GUI/YDL_GUI/main.py
@@ -0,0 +1,35 @@
+import youtube_dl
+import threading
+from ttkbootstrap import *
+from tkinter import *
+
+
+root = Tk()
+root.title('Youtube-DL GUI')
+style = Style(theme='cosmo')
+root.resizable(width=False, height=False)
+
+
+url_entry = Entry(root, width=35, borderwidth=5)
+working_label = Label(root, text='')
+
+
+def main():
+    url = url_entry.get()
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+def start_thread():
+    threading.Thread(target=main).start()
+
+
+run_button = Button(root, text='Run', width=20, borderwidth=5, command=start_thread)
+
+
+url_entry.grid(row=0, column=0, columnspan=3)
+working_label.grid(row=1, column=1)
+run_button.grid(row=1, column=1)
+
+
+root.mainloop()
