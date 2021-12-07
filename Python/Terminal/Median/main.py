75744ed283de46ee660fc3a6eab99f8ce363cde8
Solmyr77 <91564294+Solmyr77@users.noreply.github.com>
2021-12-08 00:13:09 +0100

    Update/Sync

---
+++ Python/Terminal/Median/main.py
@@ -0,0 +1,22 @@
+import random
+
+
+class TicTacToe:
+
+    def __init__(self):
+        self.board = []
+
+    def create_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append('-')
+            self.board.append(row)
+
+    def fix_spot(self, row, col, player):
+        self.board[row][col] = player
+
+
+tic_tac_toe = TicTacToe()
+tic_tac_toe.create_board()
+print (tic_tac_toe.board)
